e252c793827de0310e2f93c97a4a8c94183c9b2b
Pavel Harelik <pasha.harelik@gmail.com>
2022-04-11 12:48:25 +0300

    Delete tests

--- tests/capitalize.test.js
+++
@@ -1,7 +0,0 @@
-import capitalize from "../src/capitalize.js";
-import assert from "assert";
-
-assert(capitalize("hello") === "Hello");
-assert(capitalize("") === "");
-
-console.log("Все тесты пройдены!");
